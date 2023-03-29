8ec7db3a97fbd81371a811088d541e17dbecec76
TingTing <ting@rytass.com>
2023-03-29 21:07:19 +0800

    refactor: content layout config

--- apps/client/core/layout/ContentLayout/index.tsx
+++ apps/client/core/layout/ContentLayout/index.tsx
@@ -5,16 +5,24 @@ import classes from './index.module.scss';
 const ContentLayout: FC = () => {
   return (
     <div className={classes.root}>
-      <Typography variant="h2" color="text-primary">
-        When To Use
-      </Typography>
-      <Quotation>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat orci egestas tristique pretium. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </Quotation>
-      <Typography variant="body1" color="text-primary">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat orci egestas tristique pretium. Vivamus vitae odio diam. Nam at enim purus.
-      </Typography>
-      <Typography variant="body1" color="text-primary">
-        Praesent molestie enim et sem bibendum, id sagittis velit volutpat. Sed nec magna vehicula, nulla semper velit venenatis ante et, rhoncus odio.
-      </Typography>
+      <div className={classes.content}>
+        <Typography variant="h2" color="text-primary">
+          When To Use
+        </Typography>
+        <Quotation>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat orci egestas tristique pretium. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </Quotation>
+        <Typography variant="body1" color="text-primary">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce placerat orci egestas tristique pretium. Vivamus vitae odio diam. Nam at enim purus.
+        </Typography>
+        <Typography variant="body1" color="text-primary">
+          Praesent molestie enim et sem bibendum, id sagittis velit volutpat. Sed nec magna vehicula, nulla semper velit venenatis ante et, rhoncus odio.
+        </Typography>
+        <Typography variant="h2" color="text-primary">
+          Apperance and Styles
+        </Typography>
+        <Typography variant="body1" color="text-primary">
+          Primarily for use in forms, checkboxes are used to collect input from users. Users can select a number of options ranging from zero to multiple options.
+        </Typography>
+      </div>
     </div>
   );
 }
