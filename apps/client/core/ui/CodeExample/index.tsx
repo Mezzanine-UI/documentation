416639a1bb7db416163c487328e9a3575fa5c794
TingTing <ting@rytass.com>
2023-03-31 14:52:54 +0800

    feat: def icons

--- apps/client/core/ui/CodeExample/index.tsx
+++ apps/client/core/ui/CodeExample/index.tsx
@@ -1,6 +1,60 @@
+import { useState } from 'react';
+import {
+  Button,
+  Icon,
+  IconButton,
+} from '@mezzanine-ui/react';
+import {
+  ChevronUpIcon,
+  ChevronDownIcon,
+} from '@mezzanine-ui/icons';
+import {
+  CopyIcon,
+  MoonIcon,
+  SunIcon,
+} from '@public/icons';
+import classes from './index.module.scss';
+
 const CodeExample: FC = () => {
+  const [isShown, setIsShown] = useState(false);
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <div>CodeExample</div>
+    <div className={classes.root}>
+      <div className={classes.componentWrapper}>
+        預設文字
+      </div>
+      <div className={classes.actionsWrapper}>
+        <Button
+          type="button"
+          prefix={(
+            <Icon icon={isShown ? ChevronUpIcon:  ChevronDownIcon} />
+          )}
+          onClick={() => {
+            setIsShown(state => !state);
+          }}
+        >
+          {isShown ? 'Hide Code' : 'Show Code'}
+        </Button>
+        <div>
+          <IconButton
+            type="button"
+            size="large"
+            onClick={() => {
+              setIsDark(state => !state);
+            }}
+          >
+            <Icon icon={isDark ? SunIcon : MoonIcon} />
+          </IconButton>
+          <IconButton
+            type="button"
+            size="large"
+          >
+            <Icon icon={CopyIcon} />
+          </IconButton>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ apps/client/public/icons/copy.tsx
@@ -0,0 +1,15 @@
+import { IconDefinition } from '@mezzanine-ui/icons';
+
+export const CopyIcon: IconDefinition = {
+  name: 'copy',
+  definition: {
+    svg: {
+      viewBox: '0 0 24 24',
+    },
+    path: {
+      fill: 'currentColor',
+      fillRule: 'evenodd',
+      d: 'M9.08209 5.5H18.5V14.9179H9.08209V5.5ZM7.58209 4H9.08209H18.5H20V5.5V14.9179V16.4179H18.5H17.8507V20H4V6.14925H7.58209V5.5V4ZM7.58209 7.58209H5.43284V18.5672H16.4179V16.4179H9.08209H7.58209V14.9179V7.58209Z',
+    },
+  },
+};
